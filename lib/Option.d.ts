b6e1c7e1caf975236ef9f043b05096dc304d7e7c
Anders Olav Candasamy <anders.can1@gmail.com>
2017-08-13 18:12:20 +0200

    mend

--- lib/Option.d.ts
+++ lib/Option.d.ts
@@ -1,4 +1,14 @@
 export declare abstract class Option<A> {
+    /**
+     * Smart constructor for Options.
+     * Default predicate returns None iff `ele` is falsy else Some(ele)
+     *
+     * @param element The element to convert to a Option
+     * @param predicate Optional predicate to determine if ele is None or Some
+     * @return {Option} returns a `None` for all falsy values
+     */
+    static from<E>(element: E, predicate?: (e: E) => boolean): Option<E>;
+    toString(): string;
     protected abstract _isSome(): boolean;
     map<B>(fn: (a: A) => B): Option<B>;
     flatMap<B>(fn: (a: A) => Option<B>): Option<B>;
@@ -13,7 +23,7 @@ export declare class None extends Option<never> {
 }
 export declare const singletonNone: None;
 export declare class Some<A> extends Option<A> {
-    value: A;
+    readonly value: A;
     constructor(value: A);
     protected _isSome(): boolean;
     get(): A;
