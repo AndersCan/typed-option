de6580fbeed9fb6e5ba067f72cf3287c519b3159
Anders Olav Candasamy <anders.can1@gmail.com>
2017-12-04 19:53:52 +0100

    update lib

--- lib/Option.d.ts
+++ lib/Option.d.ts
@@ -14,15 +14,55 @@ export declare abstract class Option<A> {
      * @return {Option} returns a `None` for all falsy values
      */
     static from<E>(element: E | undefined, predicate?: (e: E) => boolean): Option<E>;
+    /**
+     * Combines two options into a new option.
+     * Curried for easier integration with `[].reduce`
+     * @param fn function that combines two Options
+     */
+    static combine<A, B, C>(fn: (a: A, b: B) => C): (a: Option<A>, b: Option<B>) => Option<C>;
     toString(): string;
+    /**
+     * Runs function `fn` with the current value
+     * @param fn function to apply to value
+     * @returns this
+     */
     do(fn: (a: A) => void): this;
+    /**
+     * Maps the current value to a new value.
+     * @param fn function that `map`s the current value to a new value
+     * @returns Some(newValue) or None
+     */
     map<B>(fn: (a: A) => B | undefined): Option<B>;
+    /**
+     * Same as `map`, but for when `fn` returns a Option. `flatMap` removes the nested `Option`
+     * @param fn function that returns a Option
+     * @returns Some(newValue) or None
+     */
     flatMap<B>(fn: (a: A) => Option<B>): Option<B>;
+    /**
+     * Some - returns the current value.
+     * None - returns the given `input`.
+     * @param input function or constant that is returned if `this` is `None`
+     */
     getOrElse<B>(fn: (() => B)): A | B;
     getOrElse<B>(constant: B): A | B;
+    /**
+     * Some - returns `this`.
+     * None - returns the given `input`.
+     * @param input function or constant that is returned if `this` is `None`
+     */
     orElse<B>(fn: () => Option<B>): Option<A | B>;
     orElse<B>(constant: Option<B>): Option<A | B>;
+    /**
+     * Tests that the current value holds the `fn` predicate
+     * @param fn predicate function to test current value
+     * @returns `this` if predicate holds else None
+     */
     filter(fn: (a: A) => boolean): Option<A>;
+    /**
+     * A `pattern matching` syntax on Options
+     * @param matcher object with keys `none` and `some`
+     */
     match<R1, R2 = R1>(matcher: Matchers<R1, A, R2>): R1 | R2;
     isNone(): this is None;
     isSome(): this is Some<A>;
